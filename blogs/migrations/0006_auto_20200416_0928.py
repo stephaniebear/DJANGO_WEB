67ed77bdab03dcfe9404a5f95a9ca8f087f62748
darkshamanking@hotmail.com <darkshamanking@hotmail.com>
2020-05-25 14:14:06 +0700

    fix migrate_1

--- blogs/migrations/0006_auto_20200416_0928.py
+++ blogs/migrations/0006_auto_20200416_0928.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='result',
             name='id',
-            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False),
+            
         ),
     ]
