74bf664c823f39e3f4548ac0c03a4dbd3e755008
darkshamanking@hotmail.com <darkshamanking@hotmail.com>
2020-05-25 13:49:13 +0700

    add database url

--- DJANGO_PROJECT/settings.py
+++ DJANGO_PROJECT/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(default='ec2-34-200-72-77.compute-1.amazonaws.com'}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
